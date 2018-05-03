8bedbbc42d4f09033bdea3d6c2f8fb2ad9840827
Alejandro Zielinsky <azielinsky95@gmail.com>
2018-05-03 15:39:00 -0400

    Initial Commit

---
+++ iOS-Midterm-Sequencer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS-Midterm-Sequencer
+//
+//  Created by Alejandro Zielinsky on 2018-05-03.
+//  Copyright © 2018 Alejandro Zielinsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
